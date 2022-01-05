26596ad554f3cea4e4ac570cb4f87810a2161b35
Huy <truong.huy96@gmail.com>
2022-01-05 16:00:32 +0700

    final 5 bai

---
+++ js/Bai2.js
@@ -0,0 +1,31 @@
+/** Tính giá trị trung bình
+ * Khôi 1: Input
+ * num1, num2, num3, num4, num5
+ *  
+ * Khối 2: Các bước xử lý
+ *  B1: khai báo biến
+ * num1, num2, num3, num4, num5
+ * 
+ *  B2: Lập công thức tính toán 
+ * avgValue = (num1 + num2 + num3 + num4 + num5)/5;
+ * 
+ *  B3: hiển thị kết quả => console.log
+ * 
+ * Khôi 3: Output
+ * avgValue
+ * 
+ */
+
+// B1
+var num1 = 3.2;
+var num2 = 1.4;
+var num3 = 5.6;
+var num4 = 4.8;
+var num5 = 1.1;
+var avgValue = 0;
+
+// B2
+avgValue = (num1 + num2 + num3 + num4 + num5)/5;
+
+// B3
+console.log("Giá trị trung bình: "+avgValue);
